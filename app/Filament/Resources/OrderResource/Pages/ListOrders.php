ad60d73b39949cb02bfbd6fa09434e5c97f1036c
sitimarfungahdwil <sitimarfungahdl@gmail.com>
2025-10-15 17:18:25 +0700

    Adding Order Stats and Filter Order

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,8 +3,13 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Models\Order;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+
+
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
+
 
 class ListOrders extends ListRecords
 {
@@ -16,4 +21,30 @@ class ListOrders extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderStats::class
+        ];
+    }
+
+    public function getTabs(): array
+{
+    return [
+        'all' => ListRecords\Tab::make('All'),
+
+        'processing' => ListRecords\Tab::make('Processing')
+            ->modifyQueryUsing(fn ($query) => $query->where('status', 'processing')),
+
+        'shipped' => ListRecords\Tab::make('Shipped')
+            ->modifyQueryUsing(fn ($query) => $query->where('status', 'shipped')),
+        'delivered' => ListRecords\Tab::make('Delivered')
+            ->modifyQueryUsing(fn ($query) => $query->where('status', 'delivered')),
+        'canceled' => ListRecords\Tab::make('Canceled')
+            ->modifyQueryUsing(fn ($query) => $query->where('status', 'canceled')), 
+    ];
+}
+
+
 }
